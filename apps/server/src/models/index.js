de33cffb88127fb3f64e3c3cce0f6bc44af30db6
JuanPabloFloresDiaz <pablojuanfd@gmail.com>
2025-08-31 21:30:16 -0600

    Agregar archivo index.js para establecer la conexión con la base de datos utilizando Sequelize

---
+++ apps/server/src/models/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+
+const config = require('../../config/database');
+
+const basename = path.basename(__filename);
+const db = {};
+
+/*
+ Sequelize instance that allows us to establish a connection with our database engine
+ Passing connection parameters independently
+*/
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+fs
+  .readdirSync(__dirname)
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
